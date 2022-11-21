35181ca5564893f28c2b259e4adf8f790a66f7e8
johnzion <43522894+johnzion@users.noreply.github.com>
2022-11-21 15:42:26 +0100

    Create release.config.js

---
+++ release.config.js
@@ -0,0 +1,5 @@
+module.exports ={
+    branches: "main",
+    repositoryUrl: "https://github.com/johnzion/ace-iac-day-zero",
+    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
+};
